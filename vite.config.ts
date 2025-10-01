aa9d2f97bc640c1fb3dfcbe21f67dbc9364119d7
santiolaciregui <54482877+santiolaciregui@users.noreply.github.com>
2025-10-01 02:01:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.API_URL': JSON.stringify(env.API_URL)
-        
       },
       resolve: {
         alias: {
